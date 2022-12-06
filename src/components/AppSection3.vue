482e17ca3e5215246aa1c7dc368d02e0fc3c4a82
nembo93 <francesco.c193@libero.it>
2022-12-06 08:32:21 +0100

    Aggiunge elementi section3

--- src/components/AppSection3.vue
+++ src/components/AppSection3.vue
@@ -5,8 +5,16 @@ export default {
 </script>
 
 <template>
-    <h1>section3</h1>
+    <section class="sec3">
+        <div>
+            <img src="../assets/testimonials-standard-2.png" class="disp_block mg_auto" alt="">
+            <p class="mg_auto txt_center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium distinctio tempore eum eligendi mollitia, odio sit soluta. Nostrum tempore a placeat voluptas ea tempora, atque deleniti, exercitationem, sint similique sapiente?</p>
+        </div>
+    </section>
 </template>
 
 <style>
+.sec3{
+    background-image: url(../assets/interactive-map-slider-img-1.png);
+}
 </style>
